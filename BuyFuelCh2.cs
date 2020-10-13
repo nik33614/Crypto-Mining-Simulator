4c11320adf398fce27a56c15d188b785b4c8e488
Nikita <58114134+nik33614@users.noreply.github.com>
2020-10-13 21:35:12 +0300

    Add files via upload

---
+++ BuyFuelCh2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyFuelCh2 : MonoBehaviour
+{
+    int co2upgrade;
+    int cost;
+    int balance;
+    float timer;
+    public GameObject panel;
+
+    public void buy()
+    {
+        
+       
+        
+            co2upgrade = PlayerPrefs.GetInt("runupgrade2");
+            cost = PlayerPrefs.GetInt("costrunupgrade2");
+            balance = PlayerPrefs.GetInt("money");
+            timer = PlayerPrefs.GetFloat("run2");
+            Debug.Log(co2upgrade);
+        if (balance >= cost)
+        {
+
+                balance -= cost;
+                timer += 0.01f;
+                co2upgrade += 1;
+            Debug.Log(co2upgrade);
+            PlayerPrefs.SetInt("runupgrade2", co2upgrade);
+                
+                PlayerPrefs.SetInt("money", balance);
+                PlayerPrefs.SetFloat("run2", timer);
+
+
+        }
+        else
+        {
+
+                panel.SetActive(true);
+
+        }
+        
+    }
+}
---
+++ TextBuyGraCh2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBuyGraCh2 : MonoBehaviour
+{
+    int jetupgrade;
+    int cost;
+    public UnityEngine.UI.Text ScoresText;
+    public GameObject start;
+    public GameObject starts;
+    void Update()
+    {
+
+        jetupgrade = PlayerPrefs.GetInt("gravityupgrade2");
+        
+
+        if (jetupgrade == 0)
+        {
+            cost = 6;
+
+            
+
+            string a = cost.ToString();
+
+            a = a + " $";
+
+            ScoresText.text = a;
+
+            PlayerPrefs.SetInt("costgravityupgrade2", cost);
+        }
+
+        else
+        {
+            int z = PlayerPrefs.GetInt("character");
+            if (z == 1 && jetupgrade >= 10)
+            {
+                start.SetActive(false);
+                starts.SetActive(true);
+            }
+            //if (z == 1 && jetupgrade == 10)
+            //{
+            //    start.SetActive(false);
+            //    starts.SetActive(true);
+            //}
+            if (z == 2 && jetupgrade == 15)
+            {
+                start.SetActive(false);
+                starts.SetActive(true);
+            }
+            else
+            {
+                
+
+                cost = jetupgrade + 2;
+
+                cost = cost * 13;
+
+                string a = cost.ToString();
+
+                a = a + " $";
+
+                ScoresText.text = a;
+                PlayerPrefs.SetInt("costgravityupgrade2", cost);
+            }
+            
+            
+        }
+    }
+}
