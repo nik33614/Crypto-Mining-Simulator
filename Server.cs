b54804c09c20a1cae8132debd56056e8ec1e8c39
Nikita <58114134+nik33614@users.noreply.github.com>
2020-12-28 16:04:48 +0300

    Add files via upload

---
+++ Server.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server : MonoBehaviour
+{
+	public string money = "0987654";
+    public string phone = "trtryyr";
+
+    void Start()
+    {
+        StartCoroutine(GET());
+        StartCoroutine(Vovod());
+    }
+   
+    private IEnumerator Vovod()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("cost1", 2);
+        
+
+        WWW www = new WWW("http://doublenimak.ru/Test.php", form);
+
+        yield return www;
+        if (www.error != null)
+        {
+
+            Debug.Log("Произошла ошибка: " + www.error);
+            yield break;
+
+
+        }
+
+        else
+        {
+
+            Debug.Log(www.text);
+
+        }
+    }
+
+    public IEnumerator GET()
+    {
+	    int data1 = 2;
+	    
+	    WWW Query = new WWW("http://doublenimak.ru/Test.php?cost1=" + data1);
+	    yield return Query;
+	    if (Query.error != null)
+	    {
+		    Debug.Log("Server does not respond : " + Query.error);
+	    }
+	    else
+	    {
+		    if (Query.text != null) // что нам должен ответить сервер на наши данные
+		    {
+			    Debug.Log("Server responded correctly");
+		    }
+		    else
+		    {
+		    	Debug.Log("Server responded : " + Query.text);
+		    }
+	    }
+	    Query.Dispose();
+	}
+}
