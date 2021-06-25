5bdb4b14657a7e63f01809c164871d562fb2a593
Nikita <58114134+nik33614@users.noreply.github.com>
2021-06-25 20:26:55 +0300

    Update Mini_Game.cs

--- Mini_Game.cs
+++ Mini_Game.cs
@@ -39,55 +39,118 @@ public class Mini_Game : MonoBehaviour
             
             if(random == 1)
             {
-                
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 one.SetActive(true);
             }
 
             if (random == 2)
             {
-
+                one.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 two.SetActive(true);
             }
 
             if (random == 3)
             {
-
+                two.SetActive(false);
+                one.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 three.SetActive(true);
             }
 
             if (random == 4)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                one.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 four.SetActive(true);
             }
 
             if (random == 5)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                one.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 five.SetActive(true);
             }
 
             if (random == 6)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                one.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 six.SetActive(true);
             }
 
             if (random == 7)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                one.SetActive(false);
+                eight.SetActive(false);
+                nine.SetActive(false);
                 seven.SetActive(true);
             }
 
             if (random == 8)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                one.SetActive(false);
+                nine.SetActive(false);
                 eight.SetActive(true);
             }
 
             if (random == 9)
             {
-
+                two.SetActive(false);
+                three.SetActive(false);
+                four.SetActive(false);
+                five.SetActive(false);
+                six.SetActive(false);
+                seven.SetActive(false);
+                eight.SetActive(false);
+                one.SetActive(false);
                 nine.SetActive(true);
             }
         }
