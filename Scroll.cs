a57f133037f2420383848153b3f6f4f60b149882
Nikita <58114134+nik33614@users.noreply.github.com>
2022-02-14 19:28:14 +0300

    Add files via upload

---
+++ Scroll.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroll : MonoBehaviour
+{
+    public RectTransform Title;
+
+    public RectTransform content;
+    int code;
+    int type;
+    string[] subs;
+    string[] sub;
+    
+
+void Start()
+{
+    int modelsCount = 3;
+    StartCoroutine(GetInf(modelsCount, results => OnReceivedModels(results)));
+    
+}
+
+    public void UpdateItems ()
+    {
+        int modelsCount = 3;//subs.Length - 1
+        
+        
+        //StartCoroutine(GetItems(modelsCount, results => OnReceivedModels(results)));
+    }
+
+    void OnReceivedModels (TestItemModel[] models)
+    {
+        foreach (Transform child in content)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (var model in models)
+        {
+            var instance = GameObject.Instantiate(Title.gameObject) as GameObject;
+            instance.transform.SetParent(content, false);
+            InitializeItemView(instance, model);
+        }
+    }
+
+    void InitializeItemView (GameObject viewGameObject, TestItemModel model)
+    {
+        TestItemView view = new TestItemView(viewGameObject.transform);
+        Debug.Log(model.title);
+        view.titleText.text = model.title;//.Substring(1, 4);//model.title.Length
+    }
+    IEnumerator GetInf(int count, System.Action<TestItemModel[]> callback)
+    {
+        WWWForm form = new WWWForm();
+        code = PlayerPrefs.GetInt("code");
+        form.AddField("code", "000001");
+        form.AddField("type", type);
+
+        WWW www = new WWW("http://doublenikmak.ru/Mails/Mail.php", form);
+
+        yield return www;
+        if (www.error != null)
+        {
+            Debug.Log("Error: " + www.error);
+            yield break;
+            callback(null);
+        }
+        else
+        {
+            string answer = www.text;
+
+            sub = answer.Split('|');
+            var tmp = new List<string>(sub);
+            tmp.RemoveAt(0); 
+            subs  = tmp.ToArray(); 
+            
+            var results = new TestItemModel[count];
+            
+            for (int i = 0; i < count; i++)
+            {
+                
+                results[i] = new TestItemModel();
+                results[i].title = subs[i].ToString();
+                
+            }
+            
+            
+            
+
+            callback(results);
+        }
+
+    } 
+
+//    IEnumerator GetItems (int count, System.Action<TestItemModel[]> callback)
+//    {
+        
+//        var results = new TestItemModel[count];
+//        for (int i = 0; i < count; i++)
+//        {
+//            results[i] = new TestItemModel();
+//            results[i].title = subs[i].ToString();
+                
+//        }
+
+//        callback(results);
+//        return (results);
+        
+//    }
+
+    public class TestItemView
+    {
+        public Text titleText;
+        
+        public TestItemView (Transform rootView)
+        {
+            titleText = rootView.Find("TitleText").GetComponent<Text>();
+        }
+    }
+
+    public class TestItemModel
+    {
+        public string title;
+        
+    }
+}
