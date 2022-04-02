8f41fc37daae985dfdb483052b3e0e922b98490b
Nikita <58114134+nik33614@users.noreply.github.com>
2022-04-02 10:46:34 +0300

    Add files via upload

--- BuyHouse.cs
+++ BuyHouse.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class BuyHouse : MonoBehaviour
 {
@@ -9,20 +10,33 @@ public class BuyHouse : MonoBehaviour
     public string House;
     public int scene;
     public GameObject Max;
-    public GameObject Button;
-    public GmeObject Panel;
+    public GameObject Panel;
     public string Ourhouse;
-    
+
     public void BuySth()
     {
         PlayerPrefs.SetInt("Alwaysone", 1);
-        if(PlayerPrefs.GetInt(Ourhouse == 1))
+        if (PlayerPrefs.GetInt(Ourhouse) == 1)
         {
             int money = PlayerPrefs.GetInt("balancedollars");
-               if (money >= Cost)
+            if (money >= Cost)
             {
                 PlayerPrefs.SetInt(House, 1);
                 PlayerPrefs.SetInt("balancedollars", money - Cost);
+                PlayerPrefs.SetFloat("profitsec", 0);
+                PlayerPrefs.SetInt("fanupgrade", 0);
+                PlayerPrefs.SetInt("costfanupgrade", 0);
+                PlayerPrefs.SetInt("fan_timer", 7200);
+                PlayerPrefs.SetInt("Check_House", 0);
+                PlayerPrefs.SetInt("pc1",0);
+                int house_time_int = PlayerPrefs.GetInt("house_time");
+                double time_double_new = DateTime.Now.Subtract(new DateTime(2021, 3, 4)).TotalSeconds;
+                int time_new = Convert.ToInt32(Math.Round(time_double_new, 0));
+                int house_time = time_new - house_time_int;
+
+
+                
+                PlayerPrefs.SetInt("house_time", time_new);
             }
             else
             {
@@ -36,7 +50,7 @@ public class BuyHouse : MonoBehaviour
     }
     void Update()
     {
-        if(PlayerPrefs.GetInt(House)==1)
+        if (PlayerPrefs.GetInt(House) == 1)
         {
             Max.SetActive(true);
         }
