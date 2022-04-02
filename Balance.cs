0b50c39323b1d6c4249115d84c898ae8518442fe
Nikita <58114134+nik33614@users.noreply.github.com>
2022-04-02 10:46:17 +0300

    Add files via upload

--- Balance.cs
+++ Balance.cs
@@ -9,43 +9,155 @@ public class Balance : MonoBehaviour
 {
     float money;
     public UnityEngine.UI.Text ScoresText;
+    
     float profit;
     float profit_4;
-    float time = 0.001f;
-    float time_sec = 5f;
+    float time = 0.0001f;
+    float time_sec = 1f;
+    string a;
+    int len_old;
+    int len;
+    string m_s;
+    public int checker;
+    string New_;
 
     void Start()
     {
+        
         money = PlayerPrefs.GetFloat("balance");
+        m_s = money.ToString();
     }
     void Update()
     {
-        
-        time -= Time.deltaTime;
-        time_sec -= Time.deltaTime;
-        Plus();
+        if(checker == 2)
+        {
+            money = PlayerPrefs.GetFloat("balance");
+            m_s = money.ToString();
+        }
+        if (checker == 0)
+        {
+            time -= Time.deltaTime;
+            time_sec -= Time.deltaTime;
+            Plus();
+        }
+        else
+        {
+            a = Math.Round(money, 4).ToString();
+            ScoresText.text = a;
+        }
     }
     void Plus()
     {
         if (time <= 0)
         {
-            profit = PlayerPrefs.GetFloat("profitsec");
-            profit_4 = profit / 1000;
-            
-            money += profit_4;
-            string a = Math.Round(money, 4).ToString();
+            //money = PlayerPrefs.GetFloat("balance");
 
-            a = a + " ₿";
-            ScoresText.text = a;
-            time = 0.0001f;
+            string Old_ = Math.Round(money, 4).ToString();
+            //string Old_ = money.ToString();
+            int old_ = Old_.Length;
+            profit = PlayerPrefs.GetFloat("profitsec");
+            profit_4 = profit / 10;
+
+            //Debug.Log("a");
+             double pop = Math.Round((money) + profit_4, 4);
+            New_ = pop.ToString();
+            money += profit_4;
+            int new_ = New_.Length;
+
+            if (new_ < old_)
+            {
+                New_ = New_ + "0";
+                 new_ = New_.Length;
+
+                if (new_ < old_)
+                {
+                    New_ = New_ + "0";
+                     new_ = New_.Length;
+
+                    if (new_ < old_)
+                    {
+                        New_ = New_ + "0";
+                         new_ = New_.Length;
+
+                        if (new_ < old_)
+                        {
+                            New_ = New_ + "0";
+                            
+
+                        }
+                        
+                    }
+                   
+
+                }
+               
+
+            }
+            
+
+
+
+
+          ///  m_s = money.ToString();
+
+            //Debug.Log(m_s);
+            ///string len_ = m_s.Substring(0, m_s.IndexOf(","));
+            //Debug.Log(len_);
+            ///int len_1 = len_.Length + 1;//колличество чисел до запятой
+           /// a = Math.Round(money, 4).ToString();
+           /// len = a.Length;//колличество всех чисел
+          ///  len -= len_1;
+
+            
+            ///profit = PlayerPrefs.GetFloat("profitsec");
+            ///profit_4 = profit / 10;
+             //len_old = len_;
+
+             ///money += profit_4;
+             //a = Math.Round(money, 4).ToString();
+             //len = a.Length;
+            //Debug.Log(len_old);
+            //Debug.Log("1" + len);
+          ///  if(len < 4)
+          ///  {
+            ///    a = a + "0";
+           ///     len = a.Length;
+           ///     len -= len_1;
+                //Debug.Log("2" + len);
+            ///    if (len < 4)
+           ///     {
+           ///         a = a + "0";
+            ///        len = a.Length;
+           ///         len -= len_1;
+                    //  Debug.Log("3" + len);
+            ///        if (len < 4)
+            ///        {
+            // /           a = a + "0";
+            ///            len = a.Length;
+             ///           len -= len_1;
+                        //    Debug.Log("4" + len);
+              ///          if (len < 4)
+              ///          {
+               ///             a = a + "0";
+
+              ///          }
+             ///       }
+                   
+            ///    }
+                
+          ///  }
+            ///m_s = a;
+            New_ = New_ + " ₿";
+            ScoresText.text = New_;
+            time = 0.1f;
         }
         if (time_sec <= 0)
         {
             money = PlayerPrefs.GetFloat("balance");
-            string a = Math.Round(money, 4).ToString();
-            a = a + " ₿";
-            ScoresText.text = a;
-            time_sec = 5f;
+             New_ = Math.Round(money, 4).ToString();
+            New_ = New_ + " ₿";
+           ScoresText.text = New_;
+            time_sec = 10f;
         }
     }
 }
